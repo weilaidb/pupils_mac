3186d240bbf1f9651c0fa8747837d1655adc73d0
weilai <weilai@weilaideMac.local>
2019-09-15 19:41:53 +0800

    add hello project

---
+++ hello/hello/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  hello
+//
+//  Created by weilai on 19/9/10.
+//  Copyright © 2019年 weilai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ hello/helloTests/helloTests.swift
@@ -0,0 +1,36 @@
+//
+//  helloTests.swift
+//  helloTests
+//
+//  Created by weilai on 19/9/10.
+//  Copyright © 2019年 weilai. All rights reserved.
+//
+
+import XCTest
+@testable import hello
+
+class helloTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
